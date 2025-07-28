62a88268807d9a3b8af8fa1156de80bafc49b5fe
samjmerrick <samjmerrick@gmail.com>
2025-07-28 09:36:41 +0100

    Add play pause icon and respect other track playing

--- app/components/waveform.tsx
+++ app/components/waveform.tsx
@@ -2,11 +2,12 @@
 
 import { useEffect, useRef } from "react";
 import { useWavesurfer } from "@wavesurfer/react";
+import { PlayCircleIcon, PauseCircleIcon } from "@heroicons/react/24/solid";
 
 export default function WaveformPlayer({ src }: { src: string }) {
   const containerRef = useRef(null);
 
-  const { wavesurfer, isReady, isPlaying, currentTime } = useWavesurfer({
+  const { wavesurfer, isReady, isPlaying } = useWavesurfer({
     container: containerRef,
     url: src,
     waveColor: "grey",
@@ -14,15 +15,34 @@ export default function WaveformPlayer({ src }: { src: string }) {
     height: 100,
   });
 
+  useEffect(() => {
+    const handler = () => {
+      if (wavesurfer && wavesurfer.isPlaying()) {
+        wavesurfer.pause();
+      }
+    };
+
+    window.addEventListener("wavesurfer-play", handler);
+    return () => window.removeEventListener("wavesurfer-play", handler);
+  }, [wavesurfer]);
+
   const onPlayPause = () => {
-    wavesurfer && wavesurfer.playPause();
+    if (!wavesurfer) return;
+
+    if (!wavesurfer.isPlaying()) {
+      // Notify all other players to stop
+      window.dispatchEvent(new Event("wavesurfer-play"));
+    }
+
+    wavesurfer.playPause();
   };
 
   return (
-    <>
-      <div ref={containerRef} />
-
-      <button onClick={onPlayPause}>{isPlaying ? "Pause" : "Play"}</button>
-    </>
+    <div className="flex flex-row">
+      <button onClick={onPlayPause} className="w-14 mx-2 cursor-pointer">
+        {isPlaying ? <PauseCircleIcon /> : <PlayCircleIcon />}
+      </button>
+      <div ref={containerRef} className="w-full" />
+    </div>
   );
 }
